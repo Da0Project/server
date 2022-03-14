68600cad69bd067a9e13597bdb7f941efac4d2ba
seungwon2 <dandelionsw2@ewhain.net>
2022-03-14 22:29:27 +0900

    :sparkles: feat : add get api

--- user/index.js
+++ user/index.js
@@ -5,14 +5,18 @@ const request = require("request");
 
 router.get("/user", (req, res) => {
 	const options = {
-		uri: "http://public-api.solscan.io/account/",
-		qs: {
-			wallet: "99djPBRRFpXCorTCJYLfExe6qupCBkp11aUgajrd5nDg",
-		},
+		uri: "https://public-api.solscan.io/account/tokens?account=",
+		qs: { account: req.query.wallet_address },
 	};
+
+	// const address = "2znw8JBTW1w2HBNiRwsTpbunjZVZ3uiGdHUt9M72i2oN";
 	request(options, function (err, response, body) {
-		// response = result;
-		return res.status(200).send([{ response: response }]);
+		for (i = 0; i < result.length; i++) {
+			if (result[i].tokenAddress === req.query.token) {
+				return res.status(200).send([{ isMember: true }]);
+			}
+		}
+		return res.status(200).send([{ isMember: false }]);
 	});
 });
 
